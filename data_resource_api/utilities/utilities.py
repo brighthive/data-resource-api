8bfb5e22465a4f964cddb98bd60b28e6910eb7d7
Gregory Mundy <gregmundy@gmail.com>
2019-04-10 00:06:59 -0400

    feat(Utilities): pull schema details from a frictionless tableschema embedded within a data resource schema"

--- data_resource_api/utilities/utilities.py
+++ data_resource_api/utilities/utilities.py
@@ -5,35 +5,76 @@ This module contains various data processing and support utilities.
 """
 
 import os
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float, Boolean, Date, DateTime
 from alembic.config import Config
 from alembic import command, autogenerate
+from tableschema import Schema
 from data_resource_api.db import Base, engine
 from data_resource_api.config import ConfigurationFactory
 
+# Mapping of a Frictionless Table Schema to SQLAlchemy Data Type.
+#
+# Note:
+#   Only a subset of Table Schema elements are mapped to SQLAlchemy data types.
+#   See: https://frictionlessdata.io/specs/table-schema/
+TABLESCHEMA_TO_SQLALCHEMY_TYPES = {
+    'string': String,
+    'number': Float,
+    'integer': Integer,
+    'boolean': Boolean,
+    'object': None,
+    'array': None,
+    'date': Date,
+    'time': None,
+    'datetime': DateTime,
+    'year': None,
+    'yearmonth': None,
+    'duration': None,
+    'geopoint': None,
+    'geojson': None,
+    'any': String
+}
+
 
 def get_app_config():
+    """Convenience method for returning application configuration.
+
+    Returns:
+        Config: Configuration object based on the current application environment.
+
+    """
     return ConfigurationFactory.from_env()
 
 
-def create_table_from_dict():
+def get_sqlalchemy_type(data_type: str):
+    try:
+        return TABLESCHEMA_TO_SQLALCHEMY_TYPES[data_type]
+    except Exception:
+        return None
+
+
+def create_sqlalchemy_fields(fields: dict):
+    sqlalchemy_fields = {}
+    for field in fields:
+        sqlalchemy_fields[field['name']] = Column(
+            get_sqlalchemy_type(field['type']), primary_key=True)
+
+    return sqlalchemy_fields
+
+
+def create_table_from_dict(table_schema: dict, table_name: str):
     app_config = get_app_config()
     alembic_config = Config(os.path.join(app_config.ROOT_PATH, 'alembic.ini'))
-    try:
-        command.init(config=alembic_config, directory=os.path.join(
-            app_config.ROOT_PATH, 'migrations'))
-    except Exception:
-        pass
-    new_class = type('demo', (Base,), {
-        '__tablename__': 'demos',
-        'id': Column(Integer, primary_key=True),
-        'thing': Column(String),
-        'thing2': Column(String)
-    })
-    try:
-        command.revision(config=alembic_config, autogenerate=True)
-    except Exception as e:
-        print(e)
-    command.upgrade(config=alembic_config, revision="head")
-    # Base.metadata.create_all(engine)
-    return new_class
+    schema = Schema(table_schema)
+    if schema.valid:
+        fields = create_sqlalchemy_fields(table_schema['fields'])
+        fields.update({'__tablename__': table_name})
+        new_class = type(table_name, (Base,), fields)
+        try:
+            command.revision(config=alembic_config, autogenerate=True)
+            command.upgrade(config=alembic_config, revision='head')
+            return new_class
+        except Exception as e:
+            print(e)
+    else:
+        print('invalid schema')

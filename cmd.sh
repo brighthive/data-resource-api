169a8cf1b2961e8329376116fea628a016b2d4f4
Gregory Mundy <gregmundy@gmail.com>
2019-05-03 11:59:59 -0400

    chore: add a simple kill subroutine to app shell script to terminate data model manager  on exit

--- cmd.sh
+++ cmd.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+python data_model_manager_runner.py &
+DATA_MODEL_MANAGER_PID=$!
+
 if [ "$APP_ENV" == "DEVELOPMENT" ] || [ -z "$APP_ENV" ]; then
     gunicorn -w 4 -b 0.0.0.0:5000 wsgi:app --reload
 else
     gunicorn -b -w 4 0.0.0.0 wsgi:app
 fi
+
+trap "kill -9 $DATA_MODEL_MANAGER_PID" EXIT

9239904a21091aa64c233e6518569ff2e9e48f14
Logan <logan@brighthive.io>
2020-03-20 08:24:13 -0700

    Create safe json util func + test

---
+++ data_resource_api/api/v1_0_0/utils.py
@@ -0,0 +1,13 @@
+import json
+import datetime
+
+
+def unknown_field_json_converter(o):
+    if isinstance(o, datetime.datetime):
+        return str(o.isoformat()) + "Z"
+
+
+def safe_json_dumps(json_dict):
+    """This will add explicit conversions for types to json.dumps
+    """
+    return json.dumps(json_dict, default=unknown_field_json_converter)

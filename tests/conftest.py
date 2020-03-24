70b53e32241f90b2fa05aeec0c9c44709c22bd57
Ripp <31484824+loganripplinger@users.noreply.github.com>
2020-03-24 00:35:03 +0000

    Significant test speed improvement / Each test clears DB data (#68)
    * Refactor test clients and clear DB on each test.
    
    * Refactor tests to run very fast.

--- tests/conftest.py
+++ tests/conftest.py
@@ -61,11 +61,13 @@ class PostgreSQLContainer(object):
 
     def start_container(self):
         """Start PostgreSQL Container."""
-        self.stop_if_running()
+        if self.get_db_if_running():
+            return
+
         try:
             self.docker_client.images.pull(self.get_postgresql_image())
         except Exception:
-            logger.exception('Failed to start test container')
+            logger.exception('Failed to pull postgres image')
 
         self.container = self.docker_client.containers.run(
             self.get_postgresql_image(),
@@ -76,6 +78,9 @@ class PostgreSQLContainer(object):
             ports=self.db_ports)
 
     def stop_if_running(self):
+        if os.getenv('DR_LEAVE_DB', False):
+            return
+
         try:
             running = self.docker_client.containers.get(
                 self.config.CONTAINER_NAME)
@@ -87,14 +92,22 @@ class PostgreSQLContainer(object):
                 return
             raise e
 
-    def stop_container(self):
-        """Stop PostgreSQL Container."""
-        if self.container is None:
-            self.container = self.docker_client.containers.get(
+    def get_db_if_running(self):
+        """Returns None or the db
+        """
+        try:
+            return self.docker_client.containers.get(
                 self.config.CONTAINER_NAME)
+        except Exception as e:
+            if "404 Client Error: Not Found" in str(e):
+                return
 
-        if self.container is not None:
-            self.container.stop()
+
+@pytest.fixture(scope='session', autouse=True)
+def run_the_database(autouse=True):
+    postgres = PostgreSQLContainer()
+    yield postgres.start_container()
+    postgres.stop_if_running()
 
 
 def delete_migration_artifacts():
@@ -126,24 +139,30 @@ class Client():
         if len(self.schema_dicts) == 0:
             raise RuntimeError("Need at least one schema dict for test client")
 
-    def run_and_return_test_client(self):
+    def start(self):
         delete_migration_artifacts()
 
         self.initialize_test_client()
 
         try:
             self.upgrade_loop()
-            return self.app.test_client()
         except UpgradeFail:
             logger.exception("Failed to upgrade database.")
+
+    def get_test_client(self):
+        return self.app.test_client()
 
     def initialize_test_client(self):
         self.data_resource_manager = DataResourceManagerSync(
             use_local_dirs=False,
             descriptors=self.schema_dicts)
         self.app = self.data_resource_manager.create_app()
-        self.postgres = PostgreSQLContainer()
-        self.postgres.start_container()
+        # self.postgres = PostgreSQLContainer()
+        # try:
+            # self.postgres.start_container()
+        # except:
+        #     logger.exception('could not start database?')
+
         self.data_model_manager = DataModelManagerSync(
             use_local_dirs=False,
             descriptors=self.schema_dicts)
@@ -152,7 +171,7 @@ class Client():
         upgraded = False
         self.counter = 1
         self.counter_max = 10
-        exponential_time = exponential_backoff(.1, 1.25)
+        exponential_time = exponential_backoff(.1, 1.05)
 
         while not upgraded and self.counter <= self.counter_max:
             try:
@@ -175,53 +194,86 @@ class Client():
 
         if self.counter > self.counter_max:
             logger.info("Max fail reached; stopping postgres container")
-            self.postgres.stop_container()
+            # self.postgres.stop_container()
             raise UpgradeFail
 
+    def clear_database(self):
+        import contextlib
+        from data_resource_api.db.base import Base, engine
+        # from sqlalchemy import MetaData
+
+        # meta = MetaData()
+
+        with contextlib.closing(engine.connect()) as con:
+            trans = con.begin()
+            for table in reversed(Base.metadata.sorted_tables):
+                con.execute(table.delete())
+            trans.commit()
+
     def stop_container(self):
+        from data_resource_api.db.base import Base, engine
         delete_migration_artifacts()
-        self.postgres.stop_container()
+        Base.metadata.drop_all(engine)
+        with engine.connect() as con:
+            con.execute('DROP TABLE alembic_version')
+        # self.postgres.stop_container()
+
+
+def setup_client(descriptors: list):
+    client = Client(descriptors)
+    client.start()
+    yield client
+    client.stop_container()
+
+
+def clear_db_and_get_test_client(client):
+    client.clear_database()
+    yield client.get_test_client()
 
 
 @pytest.fixture(scope='module')
-def regular_client():
+def _regular_client():
     """Setup the PostgreSQL database instance and run migrations.
 
     Returns:
         client (object): The Flask test client for the application.
     """
-    client = Client([credentials_descriptor, programs_descriptor])
-    yield client.run_and_return_test_client()
-    client.stop_container()
+    yield from setup_client([credentials_descriptor, programs_descriptor])
+
+
+@pytest.fixture(scope='function')
+def regular_client(_regular_client):
+    yield from clear_db_and_get_test_client(_regular_client)
 
 
 @pytest.fixture(scope='module')
-def frameworks_skills_client():
-    client = Client([frameworks_descriptor, skills_descriptor])
-    yield client.run_and_return_test_client()
-    client.stop_container()
+def _frameworks_skills_client():
+    yield from setup_client([frameworks_descriptor, skills_descriptor])
+
+
+@pytest.fixture(scope='function')
+def frameworks_skills_client(_frameworks_skills_client):
+    yield from clear_db_and_get_test_client(_frameworks_skills_client)
 
 
 @pytest.fixture(scope='module')
-def json_client():
-    client = Client([json_descriptor])
-    yield client.run_and_return_test_client()
-    client.stop_container()
+def _json_client():
+    yield from setup_client([json_descriptor])
+
+
+@pytest.fixture(scope='function')
+def json_client(_json_client):
+    yield from clear_db_and_get_test_client(_json_client)
 
 
 @pytest.fixture(scope='module')
-def everything_client():
-    client = Client([everything_descriptor])
-    yield client.run_and_return_test_client()
-    client.stop_container()
+def _everything_client():
+    yield from setup_client([everything_descriptor])
 
 
-# @pytest.fixture(scope='module')
-# def no_db_dmm():
-#     dmm = DataModelManagerSync(
-#         use_local_dirs=False,
-#         descriptors=None)
-#     yield dmm
+@pytest.fixture(scope='function')
+def everything_client(_everything_client):
+    yield from clear_db_and_get_test_client(_everything_client)
 
 
 @pytest.fixture

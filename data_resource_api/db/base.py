6fa8c8d5c994022aaeb649bc71d95a50991a16cf
Logan <logan@brighthive.io>
2020-03-26 16:55:12 -0700

    .

--- data_resource_api/db/base.py
+++ data_resource_api/db/base.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm import sessionmaker
 from data_resource_api.config import ConfigurationFactory
 
 data_resource_config = ConfigurationFactory.from_env()
+print("Connecting to ", data_resource_config.SQLALCHEMY_DATABASE_URI)
 engine = create_engine(data_resource_config.SQLALCHEMY_DATABASE_URI)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()

1a139f3cac75d0831d65b8f43fd6dd64588e9664
Logan <logan@brighthive.io>
2020-03-26 17:00:47 -0700

    .

--- data_resource_api/db/base.py
+++ data_resource_api/db/base.py
@@ -6,9 +6,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from data_resource_api.config import ConfigurationFactory
+from data_resource_api.logging import LogFactory
+
+logger = LogFactory.get_console_logger("base")
+
 
 data_resource_config = ConfigurationFactory.from_env()
-print("Connecting to ", data_resource_config.SQLALCHEMY_DATABASE_URI)
+logger.info("Connecting to ", data_resource_config.SQLALCHEMY_DATABASE_URI)
 engine = create_engine(data_resource_config.SQLALCHEMY_DATABASE_URI)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
